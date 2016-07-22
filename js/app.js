b2415bd6c1b3720044de52d7d01aa3d49f3f4f11
Chris Gibson <cgibson@stitchfix.com>
2016-07-22 11:24:41 -0700

    Minimum react rendering.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,16 @@
-import { Socket } from 'phoenix_js'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Socket } from 'phoenix_js';
 
-let socket = new Socket('10.111.3.87/socket')
-socket.connect()
+// let socket = new Socket('10.111.3.87/socket');
+// socket.connect();
+
+class Pandemic extends React.Component {
+  render() {
+    return (
+      <h1>Pandemic</h1>
+    );
+  }
+}
+
+ReactDOM.render(<Pandemic />, document.getElementById('container'));
