3f05529c55ada4688c0a115577fdd4f0e873370d
Chris Gibson <cgibson@stitchfix.com>
2016-07-22 12:13:06 -0700

    Ping pong.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider, connect } from 'react-redux';
+import { createStore } from 'redux';
 import { Socket } from 'phoenix_js';
 
-// let socket = new Socket('10.111.3.87/socket');
-// socket.connect();
+let socket = new Socket('ws://10.111.3.87:4000/socket');
+socket.connect();
+socket.onClose(e => console.log('Closed connection'));
 
-class Pandemic extends React.Component {
-  render() {
-    return (
-      <h1>Pandemic</h1>
-    );
-  }
-}
+var channel = socket.channel('game', {});
+channel.join()
+  .receive('ok', (response) => console.log('Ok', response))
+  .receive('error', () => console.log('Connection error'));
 
-ReactDOM.render(<Pandemic />, document.getElementById('container'));
+channel.on('pong', message => console.log('On Pong', message));
+
+channel.push('ping')
+  .receive('ok', message => console.log('Ping Reply', message));
+
+// class Board extends React.Component {
+//   render() {
+//
+//   }
+// }
+//
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <h1>Pandemic</h1>
+//     );
+//   }
+// }
+//
+// const initialState = {
+//   board: 'waiting'
+// };
+//
+// function app(state = initialState, action) {
+//   switch (action.type) {
+//     default:
+//       return state;
+//   }
+// }
+//
+// let store = createStore(app);
+//
+// const mapStateToProps = (state, ownProps) => {
+//   return {
+//     board:
+//     foo: 'bar'
+//   }
+// }
+//
+// const mapDispatchToProps = (dispatch, ownProps) => {
+//   return {};
+// }
+//
+// const ConnectedApp = connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(App);
+//
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <ConnectedApp />
+//   </Provider>,
+//   document.getElementById('container'));
